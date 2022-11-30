2ef0a981e63df40c46e0dc26efdc87a366d3deef
danimaulana25 <ahmaddanimaulana80@gmail.com>
2022-11-30 10:29:54 +0700

    fixed contact landingpage

--- resources/views/landingpage/contact.blade.php
+++ resources/views/landingpage/contact.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.main')
 @section('title'){{'Agrofarm Globalindo - Contact'}} @endsection
 @section('content')
+@include('sweetalert::alert')
 <!-- Page Header Start -->
 <div class="container-fluid page-header mb-5 wow fadeIn" data-wow-delay="0.1s">
     <div class="container text-center">
@@ -32,8 +33,8 @@
                     </ul>
                 </div>
                 @endif
-                <h1 class="display-6 mb-5">Ada yang bisa dibantu?</h1>
-                <p class="mb-4 text-bold">Jika ada saran dan masukan pada produk kami, anda bisa mengisikan pada kolom formulir dibawah ini.</a></p>
+                <h1 class="display-6 mb-5">Let us know if you need help</h1>
+                <p class="mb-4 text-bold">You can contact us if there are any suggestion and offer for our products with just fill the form bellow</a></p>
 
                 <form method="post" action="/adminmessage" id="myForm" enctype="multipart/form-data">
                     @csrf
@@ -75,17 +76,18 @@
             </div>
             <div class="col-lg-6 wow fadeIn" data-wow-delay="0.5s" style="min-height: 450px;">
                 <div class="position-relative rounded overflow-hidden h-100">
-                    <iframe class="position-relative w-100 h-100" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3001156.4288297426!2d-78.01371936852176!3d42.72876761954724!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4ccc4bf0f123a5a9%3A0xddcfc6c1de189567!2sNew%20York%2C%20USA!5e0!3m2!1sen!2sbd!4v1603794290143!5m2!1sen!2sbd" frameborder="0" style="min-height: 450px; border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3954.7726931097454!2d112.52180271472469!3d-7.599699094519974!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e78734a177e9417%3A0xc264e8f872325ac1!2sAgrofarm%20Globalindo%20Investama!5e0!3m2!1sid!2sid!4v1668406396185!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
             </div>
 
             <div class="col-md-6">
                 <h1 class="fw-bold m-0" style="color: #379237;">AGRO<span class="fw-bold m-0" style="color: #3C2317;">COFFEE</span></h1>
-                <h4 class="mb-4 text-bold">Terima kasih sudah berkunjung! Jika memiliki pertanyaan seputar Agro Coffe , hubungi kami melalui kontak di halaman ini.</a></p>
+                <h4 class="mb-4 text-bold">Thanks fo visiting us! If you have any questions about Agro Coffe, contact us via the contact on this page.</a></p>
             </div>
             <div class="col-lg-3 col-md-6" style="color: #000000;">
                 <p><i class="fa fa-map-marker-alt me-3"></i>Dusun Simbarinigin, Kel/Desa Pandansari, RT 02/RW 10, Simbaran, Simbaringin, Kec. Kutorejo, Kab. Mojokerto, Jawa Timur 61383
                 </p>
+                <p><i class="fa fa-phone-alt me-3"></i>+62-857-4175-3941</p>
                 <p><i class="fa fa-phone-alt me-3"></i>+62-857-3351-9756</p>
                 <p><i class="fa fa-envelope me-3"></i>care.agrofarm@gmail.com</p>
             </div>
