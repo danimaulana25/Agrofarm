8e70b2016f47eab0a3c287963f15a9793d2416d9
danimaulana25 <ahmaddanimaulana80@gmail.com>
2022-11-30 10:31:16 +0700

    fixed home landingpage

--- resources/views/landingpage/home.blade.php
+++ resources/views/landingpage/home.blade.php
@@ -7,7 +7,7 @@
     <div id="header-carousel" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <img class="w-100" src="/assets/img/cofeehome.jpeg" alt="Image">
+                <img class="w-100" src="/assets/img/cofeehome.jpeg " alt="Image">
                 <div class="carousel-caption">
                     <div class="container">
                         <div class="row justify-content-center">
@@ -61,28 +61,40 @@
 <!-- About Start -->
 <div class="container-xxl py-5">
     <div class="container">
+        @forelse($about as $item)
         <div class="row g-5">
             <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s">
                 <div class="position-relative overflow-hidden h-100" style="min-height: 400px;">
-                    <img class="position-absolute w-100 h-100 pt-5 pe-5" src="/assets/img/about1.jpg" alt="" style="object-fit: cover;">
-                    <img class="position-absolute top-0 end-0 bg-white ps-2 pb-2" src="/assets/img/about2.jpg" alt="" style="width: 200px; height: 200px;">
+                    <img class="position-absolute w-100 h-100 pt-5 pe-5" src="@if (!$item -> image)
+                                            {{ asset('/assets/img/about/'.$item -> judul.'.jpg') }}
+                                            @else
+                                            {{asset('storage/'.$item -> image)}}
+                                          @endif" alt="" style="object-fit: cover;">
+                    <img class="position-absolute top-0 end-0 bg-white ps-2 pb-2" src="@if (!$item -> image2)
+                                            {{ asset('/assets/img/about/'.$item -> id.'.jpg') }}
+                                            @else
+                                            {{asset('storage/'.$item -> image2)}}
+                                            
+                                          @endif" alt="" style="width: 200px; height: 200px;">
                 </div>
             </div>
             <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.5s">
                 <div class="h-100">
                     <div class="d-inline-block rounded-pill bg-secondary text-primary py-1 px-3 mb-3">About Us</div>
-                    <h1 class="display-6 mb-5">PT Agrofarm Globalindo Investama</h1>
+                    <h1 class="display-6 mb-5">{{$item -> judul}}</h1>
                     <div class="bg-light border-bottom border-5 border-primary rounded p-4 mb-4">
-                        <p class="text-dark mb-2">Indonesia mempunyai hasil alam yang kaya, membuat kita berupaya untuk memberikan hasil alam Indonesia terbaik salah satunya kopi Indonesia yang memiliki ciri khas dan kualitas yang bermutu.</p>
+                        <p class="text-dark mb-2">{{$item -> head}}</p>
                     </div>
-                    <p class="mb-5">Agro Coffee adalah pemasok biji kopi Arabica dan Robusta Java dari gunung Semeru, Gunung Arjuna, Gunung Kawi dari Indonesia. Dalam mencari jenis kopi terbaik di tingkat Jawa pada ketinggian 1200 - 1300 mdpl, kami memilih kopi terbaik dengan melakukan proses tanam, petik dan panen dengan cara terbaik untuk menghasilkan hasil berkualitas untuk ditawarkan untuk pembeli.</p>
-                    <a class="btn btn-primary py-2 px-3 me-3" href="">
+                    <p>{{$item -> intro1}}</p>
+                    <p>{{$item -> intro2}}</p>
+                    <p>{{$item -> intro3}}</p>
+                    <a class="btn btn-primary py-2 px-3 me-3" href="/about">
                         Learn More
                         <div class="d-inline-flex btn-sm-square bg-white text-primary rounded-circle ms-2">
                             <i class="fa fa-arrow-right"></i>
                         </div>
                     </a>
-                    <a class="btn btn-outline-primary py-2 px-3" href="">
+                    <a class="btn btn-outline-primary py-2 px-3" href="https://wa.me/6285733519756?text=Hello,I%20want%20to%20get%20information%20about%20the%20products.">
                         Contact Us
                         <div class="d-inline-flex btn-sm-square bg-primary text-white rounded-circle ms-2">
                             <i class="fa fa-arrow-right"></i>
@@ -91,6 +103,8 @@
                 </div>
             </div>
         </div>
+        @empty
+        @endforelse
     </div>
 </div>
 <!-- About End -->
@@ -103,11 +117,16 @@
             <h1 class="display-6 mb-5">Learn More About Our Commitment</h1>
         </div>
         <div class="row g-4 justify-content-center">
+            @forelse($feature as $item)
             <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
                 <div class="service-item bg-white text-center h-100 p-4 p-xl-5">
-                    <img class="img-fluid mb-4" src="/assets/img/integrity.png" alt="">
-                    <h4 class="mb-3">Integritas</h4>
-                    <p class="mb-4">Pondasi utama kami berpegang teguh dengan mengatakan kebenaran, menepati janji dan memperlakukan orang dengan adil dan hormat untuk menjaga kepercayaan.</p>
+                    <img class="img-fluid mb-4" src="@if (!$item -> image)
+                                            {{ asset('/assets/img/feature/'.$item -> nama.'.png') }}
+                                            @else
+                                            {{asset('storage/'.$item -> image)}}
+                                          @endif" alt="">
+                    <h4 class="mb-3">{{$item -> nama}}</h4>
+                    <p class="mb-4">{{$item -> deskripsi}}</p>
                     <!-- <a class="btn btn-outline-primary px-3" href="">
                         Learn More
                         <div class="d-inline-flex btn-sm-square bg-primary text-white rounded-circle ms-2">
@@ -116,45 +135,8 @@
                     </a> -->
                 </div>
             </div>
-            <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
-                <div class="service-item bg-white text-center h-100 p-4 p-xl-5">
-                    <img class="img-fluid mb-4" src="/assets/img/quality.png" alt="">
-                    <h4 class="mb-3">Kualitas</h4>
-                    <p class="mb-4">Kualitas ditunjukkan dengan memberikan layanan terbaik, memilih biji kualitas terbaik untuk kepuasan pelanggan serta support kebutuhan pelanggan dengan memberikan kuantitas dan kualitas yang dibutuhkan.</p>
-                    <!-- <a class="btn btn-outline-primary px-3" href="">
-                        Learn More
-                        <div class="d-inline-flex btn-sm-square bg-primary text-white rounded-circle ms-2">
-                            <i class="fa fa-arrow-right"></i>
-                        </div>
-                    </a> -->
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
-                <div class="service-item bg-white text-center h-100 p-4 p-xl-5">
-                    <img class="img-fluid mb-4" src="/assets/img/commitment.png" alt="">
-                    <h4 class="mb-3">Komitmen</h4>
-                    <p class="mb-4">Berkomitmen untuk melayani sebagai hak istimewa yang diberikan kepada pelanggan dengan profesional, bertanggung jawab, tuntas dan akurat.</p>
-                    <!-- <a class="btn btn-outline-primary px-3" href="">
-                        Learn More
-                        <div class="d-inline-flex btn-sm-square bg-primary text-white rounded-circle ms-2">
-                            <i class="fa fa-arrow-right"></i>
-                        </div>
-                    </a> -->
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
-                <div class="service-item bg-white text-center h-100 p-4 p-xl-5">
-                    <img class="img-fluid mb-4" src="/assets/img/inovation.png" alt="">
-                    <h4 class="mb-3">Inovasi</h4>
-                    <p class="mb-4">Menciptakan, merancangkan dan mengembangkan produk dan layanan yang memiliki kualitas dan daya tarik yang tinggi untuk menjadi preferensi pelanggan sebagai bentuk antusiasme kami dalam inovasi.</p>
-                    <!-- <a class="btn btn-outline-primary px-3" href="">
-                        Learn More
-                        <div class="d-inline-flex btn-sm-square bg-primary text-white rounded-circle ms-2">
-                            <i class="fa fa-arrow-right"></i>
-                        </div>
-                    </a> -->
-                </div>
-            </div>
+            @empty
+            @endforelse
         </div>
     </div>
 </div>
@@ -165,32 +147,26 @@
 <div class="container-xxl bg-light my-5 py-5">
     <div class="container py-5">
         <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 500px;">
-            <div class="d-inline-block rounded-pill bg-secondary text-primary py-1 px-3 mb-3">Know More About Our Product Process</div>
-            <h1 class="display-6 mb-5">Every Child Deserves The Opportunity To Learn</h1>
+            <div class="d-inline-block rounded-pill bg-secondary text-primary py-1 px-3 mb-3">Know More About Our Method Process</div>
+            <h1 class="display-6 mb-5">Our Method Process</h1>
         </div>
         <div class="row g-4 justify-content-center">
+            @forelse($process as $item)
             <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
                 <div class="causes-item d-flex flex-column bg-white border-top border-5 border-primary rounded-top overflow-hidden h-100">
                     <div class="text-center p-4 pt-0">
                         <div class="d-inline-block bg-primary text-white rounded-bottom fs-5 pb-1 px-3 mb-4">
-                            <small>Education</small>
+                            <small>{{$item -> nama}}</small>
                         </div>
-                        <h5 class="mb-3">Education For African Children</h5>
-                        <p>Tempor erat elitr rebum at clita dolor diam ipsum sit diam amet diam et eos</p>
-                        <div class="causes-progress bg-light p-3 pt-2">
-                            <div class="d-flex justify-content-between">
-                                <p class="text-dark">$10,000 <small class="text-body">Goal</small></p>
-                                <p class="text-dark">$9,542 <small class="text-body">Raised</small></p>
-                            </div>
-                            <div class="progress">
-                                <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">
-                                    <span>90%</span>
-                                </div>
-                            </div>
-                        </div>
+                        <h5 class="mb-3">{{$item -> judul}}</h5>
+                        <p>{{$item -> deskripsi}}</p>
                     </div>
                     <div class="position-relative mt-auto">
-                        <img class="img-fluid" src="/assets/img/featureCause1.jpeg" alt="">
+                        <img class="img-fluid bg-light rounded" src="@if (!$item -> image)
+                                            {{ asset('/assets/img/home/'.$item -> nama.'.jpeg') }}
+                                            @else
+                                            {{asset('storage/'.$item -> image)}}
+                                          @endif" alt="">
                         <div class="causes-overlay">
                             <a class="btn btn-outline-primary" href="">
                                 Read More
@@ -202,72 +178,8 @@
                     </div>
                 </div>
             </div>
-            <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
-                <div class="causes-item d-flex flex-column bg-white border-top border-5 border-primary rounded-top overflow-hidden h-100">
-                    <div class="text-center p-4 pt-0">
-                        <div class="d-inline-block bg-primary text-white rounded-bottom fs-5 pb-1 px-3 mb-4">
-                            <small>Pure Water</small>
-                        </div>
-                        <h5 class="mb-3">Ensure Pure Drinking Water</h5>
-                        <p>Tempor erat elitr rebum at clita dolor diam ipsum sit diam amet diam et eos</p>
-                        <div class="causes-progress bg-light p-3 pt-2">
-                            <div class="d-flex justify-content-between">
-                                <p class="text-dark">$10,000 <small class="text-body">Goal</small></p>
-                                <p class="text-dark">$9,542 <small class="text-body">Raised</small></p>
-                            </div>
-                            <div class="progress">
-                                <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">
-                                    <span>90%</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="position-relative mt-auto">
-                        <img class="img-fluid" src="/assets/img/featureCause2.jpeg" alt="">
-                        <div class="causes-overlay">
-                            <a class="btn btn-outline-primary" href="">
-                                Read More
-                                <div class="d-inline-flex btn-sm-square bg-primary text-white rounded-circle ms-2">
-                                    <i class="fa fa-arrow-right"></i>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
-                <div class="causes-item d-flex flex-column bg-white border-top border-5 border-primary rounded-top overflow-hidden h-100">
-                    <div class="text-center p-4 pt-0">
-                        <div class="d-inline-block bg-primary text-white rounded-bottom fs-5 pb-1 px-3 mb-4">
-                            <small>Healthy Life</small>
-                        </div>
-                        <h5 class="mb-3">Ensure Medical Treatment</h5>
-                        <p>Tempor erat elitr rebum at clita dolor diam ipsum sit diam amet diam et eos</p>
-                        <div class="causes-progress bg-light p-3 pt-2">
-                            <div class="d-flex justify-content-between">
-                                <p class="text-dark">$10,000 <small class="text-body">Goal</small></p>
-                                <p class="text-dark">$9,542 <small class="text-body">Raised</small></p>
-                            </div>
-                            <div class="progress">
-                                <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">
-                                    <span>90%</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="position-relative mt-auto">
-                        <img class="img-fluid" src="/assets/img/featureCause3.jpeg" alt="">
-                        <div class="causes-overlay">
-                            <a class="btn btn-outline-primary" href="">
-                                Read More
-                                <div class="d-inline-flex btn-sm-square bg-primary text-white rounded-circle ms-2">
-                                    <i class="fa fa-arrow-right"></i>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            @empty
+            @endforelse
         </div>
     </div>
 </div>
@@ -408,7 +320,7 @@
 
 
 <!-- Prouduct Start -->
-<div class="container-xxl py-5">
+<!-- <div class="container-xxl py-5">
     <div class="container">
         <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 500px;">
             <div class="d-inline-block rounded-pill bg-secondary text-primary py-1 px-3 mb-3">Produk</div>
@@ -441,6 +353,6 @@
             </div>
         </div>
     </div>
-</div>
+</div> -->
 <!-- Product End -->
 @endsection
