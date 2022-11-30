cac306fc9e726bf59bee77241be96ffa3f992edc
danimaulana25 <ahmaddanimaulana80@gmail.com>
2022-11-30 10:30:30 +0700

    fixed feature landingpage

--- resources/views/landingpage/feature.blade.php
+++ resources/views/landingpage/feature.blade.php
@@ -23,14 +23,19 @@
     <div class="container">
         <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 500px;">
             <div class="d-inline-block rounded-pill bg-secondary text-primary py-1 px-3 mb-3">What We Do</div>
-            <h1 class="display-6 mb-5">Learn More What We Do And Get Involved</h1>
+            <h1 class="display-6 mb-5">Learn More About Our Commitment</h1>
         </div>
         <div class="row g-4 justify-content-center">
+            @forelse($feature as $item)
             <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
                 <div class="service-item bg-white text-center h-100 p-4 p-xl-5">
-                    <img class="img-fluid mb-4" src="/assets/img/integrity.png" alt="">
-                    <h4 class="mb-3">Integritas</h4>
-                    <p class="mb-4">Pondasi utama kami berpegang teguh dengan mengatakan kebenaran, menepati janji dan memperlakukan orang dengan adil dan hormat untuk menjaga kepercayaan.</p>
+                    <img class="img-fluid mb-4" src="@if (!$item -> image)
+                                            {{ asset('/assets/img/feature/'.$item -> nama.'.png') }}
+                                            @else
+                                            {{asset('storage/'.$item -> image)}}
+                                          @endif" alt="">
+                    <h4 class="mb-3">{{$item -> nama}}</h4>
+                    <p class="mb-4">{{$item -> deskripsi}}</p>
                     <!-- <a class="btn btn-outline-primary px-3" href="">
                         Learn More
                         <div class="d-inline-flex btn-sm-square bg-primary text-white rounded-circle ms-2">
@@ -39,79 +44,36 @@
                     </a> -->
                 </div>
             </div>
-            <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
-                <div class="service-item bg-white text-center h-100 p-4 p-xl-5">
-                    <img class="img-fluid mb-4" src="/assets/img/quality.png" alt="">
-                    <h4 class="mb-3">Kualitas</h4>
-                    <p class="mb-4">Kualitas ditunjukkan dengan memberikan layanan terbaik, memilih biji kualitas terbaik untuk kepuasan pelanggan serta support kebutuhan pelanggan dengan memberikan kuantitas dan kualitas yang dibutuhkan.</p>
-                    <!-- <a class="btn btn-outline-primary px-3" href="">
-                        Learn More
-                        <div class="d-inline-flex btn-sm-square bg-primary text-white rounded-circle ms-2">
-                            <i class="fa fa-arrow-right"></i>
-                        </div>
-                    </a> -->
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
-                <div class="service-item bg-white text-center h-100 p-4 p-xl-5">
-                    <img class="img-fluid mb-4" src="/assets/img/commitment.png" alt="">
-                    <h4 class="mb-3">Komitmen</h4>
-                    <p class="mb-4">Berkomitmen untuk melayani sebagai hak istimewa yang diberikan kepada pelanggan dengan profesional, bertanggung jawab, tuntas dan akurat.</p>
-                    <!-- <a class="btn btn-outline-primary px-3" href="">
-                        Learn More
-                        <div class="d-inline-flex btn-sm-square bg-primary text-white rounded-circle ms-2">
-                            <i class="fa fa-arrow-right"></i>
-                        </div>
-                    </a> -->
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
-                <div class="service-item bg-white text-center h-100 p-4 p-xl-5">
-                    <img class="img-fluid mb-4" src="/assets/img/inovation.png" alt="">
-                    <h4 class="mb-3">Inovasi</h4>
-                    <p class="mb-4">Menciptakan, merancangkan dan mengembangkan produk dan layanan yang memiliki kualitas dan daya tarik yang tinggi untuk menjadi preferensi pelanggan sebagai bentuk antusiasme kami dalam inovasi.</p>
-                    <!-- <a class="btn btn-outline-primary px-3" href="">
-                        Learn More
-                        <div class="d-inline-flex btn-sm-square bg-primary text-white rounded-circle ms-2">
-                            <i class="fa fa-arrow-right"></i>
-                        </div>
-                    </a> -->
-                </div>
-            </div>
+            @empty
+            @endforelse
         </div>
     </div>
-</div>
-<!-- Feature End -->
+</div><!-- Feature End -->
 
+<!-- Causes Start -->
 <div class="container-xxl bg-light my-5 py-5">
     <div class="container py-5">
         <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 500px;">
-            <div class="d-inline-block rounded-pill bg-secondary text-primary py-1 px-3 mb-3">Know More About Our Product Process</div>
-            <h1 class="display-6 mb-5">Every Child Deserves The Opportunity To Learn</h1>
+            <div class="d-inline-block rounded-pill bg-secondary text-primary py-1 px-3 mb-3">Know More About Our Method Process</div>
+            <h1 class="display-6 mb-5">Our Method Process</h1>
         </div>
         <div class="row g-4 justify-content-center">
+            @forelse($process as $item)
             <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
                 <div class="causes-item d-flex flex-column bg-white border-top border-5 border-primary rounded-top overflow-hidden h-100">
                     <div class="text-center p-4 pt-0">
                         <div class="d-inline-block bg-primary text-white rounded-bottom fs-5 pb-1 px-3 mb-4">
-                            <small>Education</small>
+                            <small>{{$item -> nama}}</small>
                         </div>
-                        <h5 class="mb-3">Education For African Children</h5>
-                        <p>Tempor erat elitr rebum at clita dolor diam ipsum sit diam amet diam et eos</p>
-                        <div class="causes-progress bg-light p-3 pt-2">
-                            <div class="d-flex justify-content-between">
-                                <p class="text-dark">$10,000 <small class="text-body">Goal</small></p>
-                                <p class="text-dark">$9,542 <small class="text-body">Raised</small></p>
-                            </div>
-                            <div class="progress">
-                                <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">
-                                    <span>90%</span>
-                                </div>
-                            </div>
-                        </div>
+                        <h5 class="mb-3">{{$item -> judul}}</h5>
+                        <p>{{$item -> deskripsi}}</p>
                     </div>
                     <div class="position-relative mt-auto">
-                        <img class="img-fluid" src="/assets/img/featureCause1.jpeg" alt="">
+                        <img class="img-fluid bg-light rounded" src="@if (!$item -> image)
+                                            {{ asset('/assets/img/home/'.$item -> nama.'.jpeg') }}
+                                            @else
+                                            {{asset('storage/'.$item -> image)}}
+                                          @endif" alt="">
                         <div class="causes-overlay">
                             <a class="btn btn-outline-primary" href="">
                                 Read More
@@ -123,75 +85,13 @@
                     </div>
                 </div>
             </div>
-            <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
-                <div class="causes-item d-flex flex-column bg-white border-top border-5 border-primary rounded-top overflow-hidden h-100">
-                    <div class="text-center p-4 pt-0">
-                        <div class="d-inline-block bg-primary text-white rounded-bottom fs-5 pb-1 px-3 mb-4">
-                            <small>Pure Water</small>
-                        </div>
-                        <h5 class="mb-3">Ensure Pure Drinking Water</h5>
-                        <p>Tempor erat elitr rebum at clita dolor diam ipsum sit diam amet diam et eos</p>
-                        <div class="causes-progress bg-light p-3 pt-2">
-                            <div class="d-flex justify-content-between">
-                                <p class="text-dark">$10,000 <small class="text-body">Goal</small></p>
-                                <p class="text-dark">$9,542 <small class="text-body">Raised</small></p>
-                            </div>
-                            <div class="progress">
-                                <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">
-                                    <span>90%</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="position-relative mt-auto">
-                        <img class="img-fluid" src="/assets/img/featureCause2.jpeg" alt="">
-                        <div class="causes-overlay">
-                            <a class="btn btn-outline-primary" href="">
-                                Read More
-                                <div class="d-inline-flex btn-sm-square bg-primary text-white rounded-circle ms-2">
-                                    <i class="fa fa-arrow-right"></i>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
-                <div class="causes-item d-flex flex-column bg-white border-top border-5 border-primary rounded-top overflow-hidden h-100">
-                    <div class="text-center p-4 pt-0">
-                        <div class="d-inline-block bg-primary text-white rounded-bottom fs-5 pb-1 px-3 mb-4">
-                            <small>Healthy Life</small>
-                        </div>
-                        <h5 class="mb-3">Ensure Medical Treatment</h5>
-                        <p>Tempor erat elitr rebum at clita dolor diam ipsum sit diam amet diam et eos</p>
-                        <div class="causes-progress bg-light p-3 pt-2">
-                            <div class="d-flex justify-content-between">
-                                <p class="text-dark">$10,000 <small class="text-body">Goal</small></p>
-                                <p class="text-dark">$9,542 <small class="text-body">Raised</small></p>
-                            </div>
-                            <div class="progress">
-                                <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">
-                                    <span>90%</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="position-relative mt-auto">
-                        <img class="img-fluid" src="/assets/img/featureCause3.jpeg" alt="">
-                        <div class="causes-overlay">
-                            <a class="btn btn-outline-primary" href="">
-                                Read More
-                                <div class="d-inline-flex btn-sm-square bg-primary text-white rounded-circle ms-2">
-                                    <i class="fa fa-arrow-right"></i>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            @empty
+            @endforelse
+
         </div>
     </div>
 </div>
+<!-- Causes End -->
 
 <!-- Back to Top -->
 <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
